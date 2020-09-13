c5e9f8bea6fabb36bc516751f0874534291ab3e1
MoKi <61506669+moki929@users.noreply.github.com>
2020-09-13 11:13:22 -0400

    defense

--- spawn.js
+++ spawn.js
@@ -8,4 +8,18 @@ Game.creeps['Upgrader1'].memory.role = 'upgrader';
 
 //builder
 Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1',
-    { memory: { role: 'builder' } } );
+  { memory: { role: 'builder' } } );
+
+//Large
+Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
+  'HarvesterBig',
+  { memory: { role: 'harvester' } } );
+
+//self-destruct
+Game.creeps['Harvester1'].suicide()
+
+//safe mode
+Game.spawns['Spawn1'].room.controller.activateSafeMode();
+
+//build a Defense Tower
+Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
