0e25a5b85359fdc21aebc41c6bbad0e88c446ef3
MoKi <61506669+moki929@users.noreply.github.com>
2020-09-14 08:21:16 -0400

    clean up

--- role.upgrader.js
+++ role.upgrader.js
@@ -14,11 +14,11 @@ var roleUpgrader = {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
+        }
+        else {
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-            else {
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
         }
     }
 };
