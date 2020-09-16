01ec343416869d100a0fe39f6c0ddf330cc21aed
MoKi <61506669+moki929@users.noreply.github.com>
2020-09-16 00:52:53 -0400

    reading

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,24 +1,31 @@
 var roleUpgrader = {
     run: function(creep) {
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
+        //if creep is working but runs out of energy
+        if(creep.memory.working==true && creep.carry.energy == 0) {
+            //switch state
+            creep.memory.working=false;
+            // creep.say('🔄 harvest');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
-            creep.say('upgrade!');
+        // when creep gets full of energy
+        if(creep.memory.working==false && creep.carry.energy == 50) {
+            //switch state
+            creep.memory.working=true;
+            // creep.say('upgrade!');
         } 
 
-        if(creep.memory.upgrading) {
-            var sources = creep.room.find(FIND_SOURCES);
+        //creep to transfer energy to the Controller
+        if(creep.memory.working==true) {
+            // var sources = creep.room.find(FIND_SOURCES);
+            //try to upgrade the Controller
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
+        // if creep is supposed to get more energy
         else {
+            var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});}
         }
     }
 };
