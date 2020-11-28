5f60b08dfd727fa4b16ecfb80f8ee407ab9c3917
MoKi <61506669+moki929@users.noreply.github.com>
2020-11-28 12:35:54 -0500

    300 Base energy

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ module.exports.loop = function () {
         var newName = "Upgrader" + Game.time;
         console.log("Spawning new upgrader:" + newName);
         Game.spawns["Spawn1"].spawnCreep(
-            [WORK, WORK, CARRY, MOVE, MOVE], //100*2+50*3=350
+            [WORK, WORK, CARRY, MOVE], //100*2+50*2=300
             newName,
             {
                 memory: { role: "upgrader" },
