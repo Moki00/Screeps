f83e148bd0be833ed12d5c09913ce410b41ca150
MoKi <61506669+moki929@users.noreply.github.com>
2020-11-27 23:01:43 -0500

    more move

--- role.harvester.js
+++ role.harvester.js
@@ -11,8 +11,8 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (
-                        (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
+                        (structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_EXTENSION ||
                             structure.structureType == STRUCTURE_TOWER) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
                     );
