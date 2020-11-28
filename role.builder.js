9a7945e3ca5356649cacfc3070192f18ee2c9c51
MoKi <61506669+moki929@users.noreply.github.com>
2020-11-28 10:09:50 -0500

    h

--- role.builder.js
+++ role.builder.js
@@ -20,8 +20,8 @@ var roleBuilder = {
             }
         } else {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {
                     visualizePathStyle: { stroke: "#ffaa00" },
                 });
             }
