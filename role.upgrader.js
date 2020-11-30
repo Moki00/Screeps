60f95542bfbf01c636ee6841008818d4351df916
MoKi <61506669+moki929@users.noreply.github.com>
2020-11-30 11:07:38 -0500

    moving

--- role.upgrader.js
+++ role.upgrader.js
@@ -15,8 +15,8 @@ var roleUpgrader = {
 
         if (!creep.memory.upgrading) {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {
                     visualizePathStyle: { stroke: "#ffffff" },
                 });
             }
