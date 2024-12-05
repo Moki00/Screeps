8fafcdc28265189cd977ad73c2895b4681b4d3d3
MoKi <Mokiwins@gmail.com>
2024-12-04 22:22:15 -0500

    removingFolders

--- main.js
+++ main.js
@@ -16,49 +16,49 @@ global.Logger = new QosLogger();
 
 /* Add "sos library" - https://github.com/ScreepsOS/sos-library */
 global.SOS_LIB_PREFIX = "thirdparty_";
-global.sos_lib = require("thirdparty_sos_lib");
+global.sos_lib = require("sos_lib");
 
 /* Add additional room visualizations - https://github.com/screepers/RoomVisual */
-require("thirdparty_roomvisual");
+require("roomvisual");
 
 /* Add "creep talk" library - https://github.com/screepers/creeptalk */
-const language = require("thirdparty_creeptalk_emoji");
-require("thirdparty_creeptalk")({
+const language = require("creeptalk_emoji");
+require("creeptalk")({
   public: true,
   language: language,
 });
 
 /* Make the quorum library code available globally */
-global.qlib = require("lib_loader");
+global.qlib = require("loader");
 
 /* Extend built in objects */
-require("extends_controller");
-require("extends_creep");
-require("extends_creep_actions");
-require("extends_creep_movement");
-require("extends_creep_overrides");
-require("extends_lab");
-require("extends_mineral");
-require("extends_observer");
-require("extends_room_alchemy");
-require("extends_room_conflict");
-require("extends_room_construction");
-require("extends_room_control");
-require("extends_room_economy");
-require("extends_room_intel");
-require("extends_room_logistics");
-require("extends_room_landmarks");
-require("extends_room_meta");
-require("extends_room_movement");
-require("extends_room_spawning");
-require("extends_room_structures");
-require("extends_room_territory");
-require("extends_roomposition");
-require("extends_terminal");
-require("extends_source");
-require("extends_storage");
+require("controller");
+require("creep");
+require("creep_actions");
+require("creep_movement");
+require("creep_overrides");
+require("lab");
+require("mineral");
+require("observer");
+require("room_alchemy");
+require("room_conflict");
+require("room_construction");
+require("room_control");
+require("room_economy");
+require("room_intel");
+require("room_logistics");
+require("room_landmarks");
+require("room_meta");
+require("room_movement");
+require("room_spawning");
+require("room_structures");
+require("room_territory");
+require("roomposition");
+require("terminal");
+require("source");
+require("storage");
 
-const QosKernel = require("qos_kernel");
+const QosKernel = require("kernel");
 
 module.exports.loop = function () {
   if (Game.cpu.bucket < 500) {
